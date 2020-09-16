f016cb84073064c71be49b12356cd88e397b936c
cenee <ceneemail@gmail.com>
2020-09-16 23:06:07 +0200

    Updates

---
+++ precache-manifest.28569ccab30118d7736cf2adaf8e5cc4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9173db06304aac669c15d11436a691cb",
+    "url": "/workate-gallery/index.html"
+  },
+  {
+    "revision": "a44a3d27ea494369df76",
+    "url": "/workate-gallery/static/css/2.6c8eae20.chunk.css"
+  },
+  {
+    "revision": "14b84ccc81eb1e5c8b88",
+    "url": "/workate-gallery/static/css/main.0d5cc892.chunk.css"
+  },
+  {
+    "revision": "a44a3d27ea494369df76",
+    "url": "/workate-gallery/static/js/2.69239c0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/workate-gallery/static/js/2.69239c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b84ccc81eb1e5c8b88",
+    "url": "/workate-gallery/static/js/main.5ad2ef9b.chunk.js"
+  },
+  {
+    "revision": "52e33eea8a6b4ff732c5",
+    "url": "/workate-gallery/static/js/runtime-main.530eced4.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/workate-gallery/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/workate-gallery/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/workate-gallery/static/media/revicons.2feb69cc.eot"
+  }
+]);
